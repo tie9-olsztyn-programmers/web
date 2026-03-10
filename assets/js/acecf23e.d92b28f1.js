ef5df2029ededd7f2c12abb37ec19ae69fcb779f
jarek m5 <jaroslaw.strzelecki@protonmail.com>
2026-03-10 21:58:23 +0100

    Deploy website - based on 80c5fd61bd9ef6a34377d372f0d75f156ae808bc

--- assets/js/acecf23e.d92b28f1.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkweb=globalThis.webpackChunkweb||[]).push([[1903],{1912(b){b.exports=JSON.parse('{"blogBasePath":"/web/blog","blogTitle":"Blog","authorsListPath":"/web/blog/authors"}')}}]);
